Clean-up, MNIST loader, automatic differentiation, fix for Xavier weight init. Gradient descent is non-functional.

activation.py
@@ -1,16 +1,13 @@
 """
     Module: activation.py
     This module contains a definition of various activation functions.
-
-    The functions here are marked with the "@njit" decorator, this means that they're compiled to machine code at runtime.
+    https://github.com/google/jax
 """
-import cmath
 
-from numba import njit
-from numpy import absolute
+import jax.numpy as jnp
+from jax import grad, jit
 
 
-@njit
 def sigmoid(x):
     """
         Return the activation after a sigmoid function
@@ -21,10 +18,10 @@ def sigmoid(x):
         Returns:
             The activation value.
     """
-    return 1 / (1 + absolute(cmath.exp(-x)))
+    return 1 / (1 + jnp.absolute(jnp.exp(-x)))
+sigmoid.derivative = jit(grad(sigmoid))
 
 
-@njit
 def tanh(x):
     """
         Return the activation after a hyperbolic tangent function.
@@ -35,10 +32,10 @@ def tanh(x):
         Returns:
             The activation value.
     """
-    return cmath.tanh(x).real
+    return jnp.tanh(x)
+tanh.derivative = jit(grad(tanh))
 
 
-@njit
 def RELU(x):
     """
         Return the activation after a rectifier linear unit function.
@@ -50,10 +47,10 @@ def RELU(x):
             The activation value.
     """
     return x * (x > 0)
+RELU.derivative = jit(grad(RELU))
 
 
-@njit
-def none(x):
+def Input(x):
     """
         Return the value passed without any activation function applied.
 
@@ -64,3 +61,4 @@ def none(x):
             The activation value.
     """
     return x
+Input.derivative = jit(grad(Input))

main.py
@@ -1,29 +1,25 @@
-import pynn
-from activation import *
-from derivative import *
+from pynn import Model
+import activation
+from weights import xavier_init
+from optimizer import gradient_descent
+from cost import mean_squared_loss
 import numpy as np
 
-# Loading network from file.
-#model = pynn.load_network()
+from loader import mnist
+train_data, train_labels, test_data, test_labels = mnist()
 
-# Declaration and definition of model.
-# NOTE: IF CHANGING MODEL AT ALL, YOU HAVE TO REGENERATE AND SAVE THE NETWORK.
-# THIS MEANS THAT IT'S ONLY GOOD FOR QUICK STARTUPS AND INFERENCE BUT NOT DEVELOPMENT.
-model = pynn.create_network(num_layers=5, neurons_layer=[3, 2, 3, 2, 1],
-                            activation_layer=[none, tanh, tanh, tanh, tanh], 
-                            derivative_layer=[none_derivative, tanh_derivative, tanh_derivative, tanh_derivative, tanh_derivative],
-                            weight_function="xavier",
-                            precision=np.float32)
+model = Model(precision=np.float32, weight_init=xavier_init, optimizer=gradient_descent, loss=mean_squared_loss)
+# First is input.
+model.add_layer(784, activation.Input)
+model.add_layer(20, activation.tanh)
+model.add_layer(10, activation.tanh)
 
-"""
-# Run without momentum.
-model.train(epochs=10, function="GD", inputs=[[1,1,0]]*5,
-            actual=np.ones(5), max_iter=5, learning_rate=0.1, min_precision=0.1)
-"""
+import pandas as pd
+label_series = pd.Series(train_labels)
+train_labels = np.array(pd.get_dummies(label_series).values.tolist())
+data = list(zip(train_data, train_labels))
 
-# Run with momentum.
-model.train(epochs=10, function="GD", inputs=[[0,0,1]]*5,
-            actual=np.zeros(5), max_iter=5, learning_rate=0.1, min_precision=0.1, learning_rate_function="momentum", coefficient=0.9)
+model.train(epochs=10, training_data=data, max_iter=5, learning_rate=0.1, min_precision=0.1, learning_rate_function="momentum", coefficient=0.9)
 
 # Saving network to file.
 pynn.save_network(model)

neuron.py
@@ -3,34 +3,24 @@
     This module contains a declaration and definition of the Neuron class.
 """
 
-from numpy import dot as dot_product
+from numpy import dot
 from numpy import array
+import numpy as np
 from weights import random_init, xavier_init
 
 
 class Neuron:
-    def __init__(self, num_prev, weight_function, precision):
+    def __init__(self, precision):
         """
             Constructor function for a Neuron.
 
             Args:
-                activation_for_neuron (str): The activation function for the neuron.
-                num_prev (int): The number of neurons in the previous layer.
                 precision (numpy.dtype): The numpy dtype for the network precision.
-                weight_function (str): The weight initialization function for the neuron.
         """
-        self.activation_value = precision(0)
+        self.activation = precision(0)
         self.sum = precision(0)
         self.bias = precision(0)
-
-        # Storing and initializing weights for each neuron not in the input layer.
-        if num_prev:
-            if weight_function == "xavier":
-                self.weights = xavier_init(num_prev, precision)
-            elif weight_function == "random":
-                self.weights = random_init(num_prev)
-        else:
-            self.weights = array([1], dtype=precision)
+        self.weights = None
 
     def set_sum(self, input_val):
         """
@@ -40,13 +30,4 @@ class Neuron:
             Args:
                 input_val (numpy.array): The input tensor for the neuron.
         """
-        self.sum = dot_product(input_val, self.weights) + self.bias
-
-    def set_sum_raw(self, input_val):
-        """
-            Setting the neuron sum to the input without preprocessing.
-
-            Args:
-                input_val (numpy.array): The input sum for the neuron.
-        """
-        self.sum = input_val
+        self.sum = np.dot(input_val, self.weights) + self.bias

optimizer.py
@@ -3,13 +3,12 @@
     This module contains a definition of various optimizing/learning as well as helper functions.
 """
 
-from numpy import zeros
-from training import get_activation_values, get_delta_bias_structure, get_delta_structure
-
+import numpy as np
+from itertools import repeat
 
 # WORK_IN_PROGRESS
 # TODO: Sum neuron influence over layer for multi-layer networks with more than one neuron per layer (excluding the input layer).
-def gradient_descent(self, learning_rate, loss):
+def gradient_descent(model, expected, loss, learning_rate=0.01):
     """
         Calculating the gradients for the weights and biases of each neuron and updating them.
 
@@ -18,59 +17,64 @@ def gradient_descent(self, learning_rate, loss):
 
         The bias gradient is the same except the derivative of the "activation function" so to speak is just 1. 
     """
-    # Get all uncleared activation values.
-    activation_values = get_activation_values(self=self)
+    weight_gradients = [[] for _ in range(len(model["layers"]))]
+    bias_gradients = [[] for _ in range(len(model["layers"]))]
 
-    # Get a structure to store the weight derivatives/gradients that's currently filled with the actual weights.
-    delta = get_delta_structure(self=self)
-    # Get a structure to store the bias derivatives/gradients that's currently filled with zeros.
-    delta_bias = get_delta_bias_structure(self=self)
+    output_layer = model["layers"][-1]["neurons"]
+    node_loss = [np.square(neuron.activation - actual) for neuron, actual in zip(output_layer, expected)]
 
-    for layer in reversed(range(self.num_layers_index)):
-        # Defining some indexing conveniences.
-        activations = self.model[self.neuron_activation_layer][layer]
-        derivative = self.model[self.neuron_derivative_layer][layer]
-        previous_layer = layer + 1
-        next_layer = layer - 1
+    num_layers = len(model["layers"]) - 1
+    for layer in reversed(model["layers"]):
+        # Get current layer number
+        layer_num = model["layers"].index(layer)
+        derivative = layer["activation"].derivative
 
-        # Skipping layer 0 because it doesn't have any weights to optimize.
-        if layer == 0:
-            continue
+        # Get previous layer and number.
+        prev_layer_num = layer_num - 1
+        if prev_layer_num == -1:
+            # Finished
+            return
+        else:
+            prev_layer = model["layers"][prev_layer_num]
 
-        # For each neuron in the layer.
-        for neuron in range(self.model[self.neuron_num_layer][layer]):
-            # Calculating gradients for the output layer.
-            if layer == self.num_layers:
-                # For each connection in the next layer calculate bias for which the derivative is 1.
-                for connection in range(self.model[self.neuron_num_layer][next_layer]):
-                    delta_bias[layer][neuron] = loss * 1 * \
-                        activation_values[next_layer][connection]
+        if not layer_num == num_layers:
+            next_layer_num = layer_num + 1
+            next_layer = model["layers"][next_layer_num]
 
-                # For each connection in the next layer.
-                for connection in range(self.model[self.neuron_num_layer][next_layer]):
-                    # Calculating the gradient for the weight.
-                    delta[layer][neuron][connection] = loss * \
-                        derivative(activation_values[layer][neuron]) * \
-                        activation_values[next_layer][connection]
+        # If output layer
+        if layer_num == num_layers:
+            for neuron in layer["neurons"]:
+                which_neuron = layer["neurons"].index(neuron)
+                neuron_derivative = derivative(neuron.sum)
+                gradients = []
+                for prev_neuron in prev_layer["neurons"]:
+                    gradients.append(
+                        # The loss for this neuron times the derivative of this neuron times the activation of the neuron which the weight is connecting.
+                        node_loss[which_neuron] * neuron_derivative * prev_neuron.activation
+                    )
+                weight_gradients[layer_num].append(
+                    np.asarray(gradients)
+                )
+                """
+                # Bias starts at 0, will always be 0.
+                bias_gradients[layer_num].append(
+                    [neuron.bias * node_loss[which_neuron]]
+                )
+                """
+        else:
+            for neuron in layer["neurons"]:
+                which_neuron = layer["neurons"].index(neuron)
+                neuron_derivative = derivative(neuron.sum)
+                gradients = []
+                for prev_neuron in prev_layer["neurons"]:
+                    gradients.append(
+                        # The loss for this neuron times the derivative of this neuron times the activation of the neuron which the weight is connecting.
+                        neuron_derivative * prev_neuron.activation
+                    )
+                weight_gradients[layer_num].append(
+                    np.asarray(gradients)
+                )
 
-            # Calculating gradients for the hidden layers.
-            else:
-                # For each connection in the previous layer calculate bias for which the derivative is 1.
-                for connection in range(self.model[self.neuron_num_layer][previous_layer]):
-                    delta_bias[layer][neuron] = delta[previous_layer][connection][neuron] * 1 * \
-                        activation_values[next_layer][connection]
-
-                # For each connection in the previous layer.
-                for connection in range(self.model[self.neuron_num_layer][previous_layer]):
-                    # Calculating common elements to avoid code duplication.
-                    delta[layer][neuron][connection] = delta[previous_layer][connection][neuron] * \
-                        derivative(activation_values[layer][neuron]) * \
-                        activation_values[next_layer][connection]
-
-    # Returning the calculated gradients.
-    return (delta, delta_bias)
-
-
-# WORK_IN_PROGRESS
-def stochastic_gradient_descent(self, learning_rate, loss, mini_batch):
-    pass
+    # Update rule for weight is weight = weight - (learning_rate * weight_derivative)
+    # Update rule for bias is bias = bias - (learning_rate * bias)
+    return weight_gradients, bias_gradients

pynn.py
@@ -1,32 +1,135 @@
 """
     Module: pynn.py
-    This module contains a definition of various PyNN convenience function.
+    This module contains a definition of various PyNN convenience functions for model creation, training and inference. 
 """
 
-from model import Model
 import pickle
-import image
 
-def create_network(num_layers, neurons_layer, activation_layer, derivative_layer, weight_function, precision):
-    """
-        Create new model using passed parameters
 
-        Args:
-            num_layers (int): A integer that specifies the number of layers in the network.
-            neurons_layer (list): A list containing the integer number of neurons for each layer
-            activation_layer (list): A list containing the activation function objects for each layer
-            derivative_layer (list): A list containing the derivative function objects for each layer
-            precision (numpy.dtype): The numpy dtype for the network precision.
-        
-        Returns:
-            model (object): A ready to use neural network as defined by the parameters.
-    """
-    model = Model(num_layers, precision)
-    for i in range(num_layers):
-        model.add_layer(neurons_layer[i], activation_layer[i], derivative_layer[i], weight_function)
-    # Call the model preparation function before returning the object    
-    model.compile()
-    return model
+import numpy as np
+from neuron import Neuron
+from cost import mean_squared_loss
+from training import update_weights_and_bias, get_delta_structure, get_delta_bias_structure, apply_learning_rate, calculate_momentum
+
+
+class Model:
+    def __init__(self, precision, weight_init, optimizer, loss):
+        """
+            Constructor function for a Model.
+
+            Args:
+                precision (numpy.dtype): The numpy dtype for the network precision.
+                weight_init (function): The function used for network weight initialisation.
+        """
+        self.model = {"layers": []}
+        self.precision = precision
+        self.weight_init = weight_init
+        self.optimizer = optimizer
+        self.loss = loss
+
+    def add_layer(self, num_neurons, neuron_activation):
+        """
+            Adding a layer to the model.
+
+            Args:
+                num_neurons (int): The number of neurons in the layer to be added.
+                neuron_activation (function): The activation function to use for the layer.
+        """
+
+        self.model["layers"].append(
+            {"neurons": [Neuron(self.precision) for _ in range(num_neurons)],
+            "activation": neuron_activation}
+        )
+
+        # Set number of weights to generate for each neuron in layer.
+        layer_num = len(self.model["layers"]) - 1
+        if layer_num == 0:
+            num_prev_neurons = 1
+        else:
+            # Number of neurons in previous layer
+            num_prev_neurons = len(self.model["layers"][layer_num - 1]["neurons"])
+
+        for neuron in self.model["layers"][layer_num]["neurons"]:
+            neuron.weights = self.weight_init(
+                num_prev_neurons, 
+                self.precision)
+
+    def input_layer(self, values):
+        input_layer = self.model["layers"][0]["neurons"]
+        activation = self.model["layers"][0]["activation"]
+        for neuron, value in zip(input_layer, values):
+            neuron.set_sum(value)
+            neuron.activation = activation(neuron.sum)
+
+        feed_forward = np.array([neuron.activation for neuron in input_layer], dtype=self.precision).flatten()
+        return feed_forward
+
+    def step(self, inputs):
+        """
+            Feeding a given set of inputs to the network and returning the predicted values for the output layer.
+
+            Args:
+                inputs (numpy.array): A 1-dimensional array of the inputs to feed to the input layer.
+
+            Returns:
+                feed_forward (numpy.array): A 1-dimensional array containing the activation values for the output layer neurons. 
+        """
+        # Set input layer, trigger and get outputs for feed forward.
+        feed_forward = self.input_layer(inputs)
+
+        # For each layer in the model, set inputs, get outputs.
+        for layer in self.model["layers"][1:]:
+            # Set inputs from previous layer and activate
+            for neuron in layer["neurons"]:
+                neuron.set_sum(feed_forward)
+                neuron.activation = layer["activation"](neuron.sum)
+            
+            # Get output for new layer.
+            feed_forward = np.array([neuron.activation for neuron in layer["neurons"]], dtype=self.precision).flatten()
+
+        # Return activation for last layer.
+        return feed_forward
+
+
+    def train(self, epochs, training_data, max_iter, mini_batch=10, learning_rate=0.01, min_precision=0.001,
+              learning_rate_function="none", coefficient=0.9):
+        """
+            Runs a optimizing algorithm on the neural network using a specified algorithm.
+
+            Args:
+                function (str): A string representing the optimizing function to use.
+                epochs (int): The number of times that the given example is shown to the network.
+                input (numpy.array): A 1-dimensional array of the inputs to feed to the input layer.
+                actual (numpy.array): A 1-dimensional array of the expected values for the output layer.
+                learning_rate (float): The learning rate hyperparameter for determining how quickly a network can learn.
+                max_iter (int): The maximum number of iterations ran on a given example.
+                mini_batch (int): The mini batch size for stochastic gradient descent. Warning; Unimplemented.
+                min_precision (float): The minimum change in the error of the network for iteration until it exits.
+        """
+        # Repeating training the network on the examples for N epochs.
+        for epoch in range(epochs): 
+            epoch_loss = 0
+            # For each example do...
+            for input_data, expected in training_data:
+                # Running multiple iterations for gradient descent.
+                for iteration in range(max_iter):
+                    # I'm only running it once for one example to preserve the set sums.
+                    predicted = self.step(input_data)
+
+                    # Loss for optimizers has to be a scalar so I'm choosing a sum.
+                    loss = self.loss(predicted, expected)
+                    epoch_loss += loss
+
+                    # Run training on single example.
+                    weight_gradients, bias_gradients = self.optimizer(self.model, expected, loss=loss, learning_rate=learning_rate)
+                    
+        print("EPOCH: ", epoch)
+        print("LOSS: ", epoch_loss)
+        if epoch_loss <= min_precision:
+            print("Reached desired precision.")
+            return
+        epoch_loss = 0
+
 
 def save_network(model):
     """