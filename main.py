Added dataset, profiling, code reorganisation, another loss function, actually working gradient descent and backprop, progress bar with variable and fixes for arrays vs scalars in sum.

main.py
@@ -1,25 +1,45 @@
-from pynn import Model
+from pynn import Model, save_network, load_network
 import activation
-from weights import xavier_init
+from weights import xavier_init, random_init
 from optimizer import gradient_descent
-from cost import mean_squared_loss
+from cost import mean_squared_loss, sum_squared_loss
 import numpy as np
 
 from loader import mnist
-train_data, train_labels, test_data, test_labels = mnist()
 
-model = Model(precision=np.float32, weight_init=xavier_init, optimizer=gradient_descent, loss=mean_squared_loss)
-# First is input.
-model.add_layer(784, activation.Input)
-model.add_layer(20, activation.tanh)
-model.add_layer(10, activation.tanh)
+def main():
+    train_data, train_labels, test_data, test_labels = mnist()
 
-import pandas as pd
-label_series = pd.Series(train_labels)
-train_labels = np.array(pd.get_dummies(label_series).values.tolist())
-data = list(zip(train_data, train_labels))
+    #model = load_network()
+    model = Model(precision=np.float16, weight_init=xavier_init, optimizer=gradient_descent, loss=sum_squared_loss)
+    # First is input.
+    model.add_layer(784, activation.sigmoid)
+    model.add_layer(20, activation.sigmoid)
+    model.add_layer(10, activation.sigmoid)
 
-model.train(epochs=10, training_data=data, learning_rate=0.1, min_precision=0.1)
+    import pandas as pd
+    label_series = pd.Series(train_labels)
+    train_labels = np.array(pd.get_dummies(label_series).values.tolist())
+    data = list(zip(train_data, train_labels))
 
-# Saving network to file.
-pynn.save_network(model)
+    model.train(epochs=1, training_data=data, learning_rate=0.01, min_precision=0.1)
+
+    # Saving network to file.
+    save_network(model)
+
+if __name__ == '__main__':
+    do_profiling = False
+    if do_profiling:
+        import cProfile
+        from pstats import Stats, SortKey
+        with cProfile.Profile() as pr:
+            main()
+
+        with open('profiling_stats.txt', 'w') as stream:
+            stats = Stats(pr, stream=stream)
+            stats.strip_dirs()
+            stats.sort_stats('time')
+            stats.dump_stats('.prof_stats')
+            stats.print_stats()
+    else:
+        main()

neuron.py
@@ -30,4 +30,4 @@ class Neuron:
             Args:
                 input_val (numpy.array): The input tensor for the neuron.
         """
-        self.sum = np.dot(self.weights, input_val) + self.bias
+        self.sum = np.add(np.dot(self.weights, input_val), self.bias).item(0)

optimizer.py
@@ -7,7 +7,7 @@ import numpy as np
 from itertools import repeat
 
 # WORK_IN_PROGRESS
-def gradient_descent(model, expected, loss):
+def gradient_descent(model, input_data, loss):
     """
         Calculating the gradients for the weights and biases of each neuron. 
     """
@@ -15,6 +15,66 @@ def gradient_descent(model, expected, loss):
     weight_gradients = [[] for _ in range(len(model["layers"]))]
     bias_gradients = [[] for _ in range(len(model["layers"]))]
 
-    # Update rule for weight is weight = weight - (learning_rate * weight_derivative)
-    # Update rule for bias is bias = bias - (learning_rate * bias)
-    return {"weights": weight_gradients}#, "bias": bias_gradients}
+    # So the same function for output and hidden can be used.
+    neuron_error.append([loss])
+
+    
+    for layer_num, layer in reversed(list(enumerate(model["layers"]))):
+        prev_layer_neurons = model["layers"][layer_num - 1]["neurons"]
+
+        # Hidden or output layers, the error is substituted by the neuron_error:
+        if layer != model["layers"][0]:
+            summed_influence = sum(neuron_error[layer_num+1])
+            for num_neuron, neuron in enumerate(layer["neurons"]):
+                summed_influence_derivative = layer["activation"].derivative(neuron.sum) * summed_influence 
+
+                neuron_weight_error = [
+                    neuron_back.activation * summed_influence_derivative for neuron_back in prev_layer_neurons
+                ]
+
+                # Only one per neuron
+                bias_gradients[layer_num].append(
+                    # The derivative for bias is 1 and that's the same as multiplying by nothing.
+                    neuron.activation * summed_influence
+                )
+
+                weight_gradients[layer_num].append(neuron_weight_error)
+                neuron_error[layer_num].append(sum(neuron_weight_error))
+
+        # Input layer (only one weight)
+        else:
+            summed_influence = sum(neuron_error[layer_num+1])
+            for num_neuron, neuron in enumerate(layer["neurons"]): 
+                summed_influence_derivative = layer["activation"].derivative(neuron.sum) * summed_influence 
+
+                neuron_weight_error = [
+                    input_data[num_neuron] * summed_influence_derivative
+                ]
+
+                # Only one per neuron
+                bias_gradients[layer_num].append(
+                    # The derivative for bias is 1 and that's the same as multiplying by nothing.
+                    neuron.activation * summed_influence
+                )
+
+                weight_gradients[layer_num].append(neuron_weight_error)
+                neuron_error[layer_num].append(sum(neuron_weight_error))
+
+    return weight_gradients, bias_gradients
+
+
+def update_weights_and_bias(model, weight_gradients, bias_gradients, learning_rate=0.01):
+    """
+        Updating the neuron weights and bias for each layer using the calculated gradients.
+
+        Args:
+            model (dict): A python dictionary that contains the model.
+            gradients (dict): A python dictionary that contains the weight and bias gradients.
+
+    """
+    for layer_num, layer in reversed(list(enumerate(model["layers"]))):
+        for neuron_num, neuron in enumerate(layer["neurons"]):
+            neuron_weight_gradients = np.array(weight_gradients[layer_num][neuron_num])
+            neuron_weight_gradients = np.multiply(neuron_weight_gradients, learning_rate)
+            neuron.weights = np.subtract(neuron.weights, neuron_weight_gradients)
+            neuron.bias = neuron.bias - bias_gradients[layer_num][neuron_num] * learning_rate